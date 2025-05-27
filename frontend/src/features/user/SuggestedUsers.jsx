0bdb1851b477160c9592560d7bb7a1deec8525e6
Sahand-feizi <sahandfeizi87@gmail.com>
2025-05-27 14:00:18 +0330

    handel error

--- frontend/src/features/user/SuggestedUsers.jsx
+++ frontend/src/features/user/SuggestedUsers.jsx
@@ -22,7 +22,7 @@ function SuggestedUsers() {
     return (
         <div className='rounded-xl bg-secondary-0 hidden lg:block sticky top-0 p-2 right-0 max-h-[17.5rem] space-y-4 ml-4'>
             <h3 className='text-[1rem] font-bold text-secondary-900 text-center'>Who to follow</h3>
-            {
+            {/* {
                 (suggestedUsers?.length > 0 && !isLoading) ? (
                     suggestedUsers.map(user => (
                         <SuggestedUser key={user._id} user={user} />
@@ -30,7 +30,7 @@ function SuggestedUsers() {
                 ) : (
                     <h1 className='text-secondary-900 text-lg font-bold my-4 text-center'>No Users To Follow 🤔</h1>
                 )
-            }
+            } */}
         </div>
     )
 }
